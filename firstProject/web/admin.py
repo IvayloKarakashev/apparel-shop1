a7ac684e74a5785163c7c0832af80b1dd1b5c299
Ivaylо <ivaylo.a.karakashev@gmail.com>
2022-10-23 13:32:29 +0300

    Registered models in admin

--- firstProject/web/admin.py
+++ firstProject/web/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from firstProject.web.models import Product, OrderItem, Order
+from firstProject.accounts.models import Profile
+from firstProject.web.models import Product, OrderItem, Order, User
 
-admin.site.register(Product)
-admin.site.register(OrderItem)
+admin.site.register(User)
+admin.site.register(Profile)
 admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Product)
