d12859a58eb67cffaa9b7398d36b4843a7cf0591
Ivaylо <ivaylo.a.karakashev@gmail.com>
2023-03-05 20:02:50 +0200

    Testing environment variables

--- firstProject/settings.py
+++ firstProject/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'firstProject.wsgi.application'
 # }
 
 DATABASES = {
-    'default': 'postgres://render_db_zrws_user:KTbKd8jx92OGh4DTBEOqsX3lvRrc488q@dpg-cfkibnhmbjsn9ecjuigg-a.frankfurt-postgres.render.com/render_db_zrws'
+    'default': dj_database_url.parse('postgres://render_db_zrws_user:KTbKd8jx92OGh4DTBEOqsX3lvRrc488q@dpg-cfkibnhmbjsn9ecjuigg-a.frankfurt-postgres.render.com/render_db_zrws')
 }
 
 
