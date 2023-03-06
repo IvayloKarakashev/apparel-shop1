ef8f54a067029c7dffd5be41c5cc53c66acc10db
Ivaylо <ivaylo.a.karakashev@gmail.com>
2023-03-06 22:23:05 +0200

    Fixing Render DB connection

--- firstProject/settings.py
+++ firstProject/settings.py
@@ -83,10 +83,20 @@ WSGI_APPLICATION = 'firstProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': dj_database_url.parse('postgres://render_db_zrws_user:KTbKd8jx92OGh4DTBEOqsX3lvRrc488q@dpg-cfkibnhmbjsn9ecjuigg-a.frankfurt-postgres.render.com/render_db_zrws')
+# }
+
 DATABASES = {
-    'default': dj_database_url.parse('postgres://render_db_zrws_user:KTbKd8jx92OGh4DTBEOqsX3lvRrc488q@dpg-cfkibnhmbjsn9ecjuigg-a.frankfurt-postgres.render.com/render_db_zrws')
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'render_db_zrws',
+        'USER': 'render_db_zrws_user',
+        'PASSWORD': 'KTbKd8jx92OGh4DTBEOqsX3lvRrc488q',
+        'HOST': 'dpg-cfkibnhmbjsn9ecjuigg-a',
+        'PORT': '5432',
+    }
 }
-a = 5
 
 
 # DATABASES = {
