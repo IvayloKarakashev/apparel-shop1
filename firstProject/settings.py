f901943dcc061fe44cd9b6420718c8c862d0b8f6
Ivaylо <ivaylo.a.karakashev@gmail.com>
2023-03-07 14:39:01 +0200

    Adding secret variables

--- firstProject/settings.py
+++ firstProject/settings.py
@@ -30,7 +30,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+                '127.0.0.1',
+                'apparel-shop1.onrender.com',
+]
 
 # Application definition
 INSTALLED_APPS = [
