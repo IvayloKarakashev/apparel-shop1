ac3c95ffa32fd61915e3347390a9e19c795ebd91
Ivaylо <ivaylo.a.karakashev@gmail.com>
2023-03-07 15:50:47 +0200

    Adding secret variables

--- firstProject/settings.py
+++ firstProject/settings.py
@@ -30,13 +30,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+LOCAL = True
 
-if DEBUG:
+if LOCAL:
     load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
+
+DEBUG = os.environ.get('DEBUG')
+
 
 ALLOWED_HOSTS = [
                 '127.0.0.1',
