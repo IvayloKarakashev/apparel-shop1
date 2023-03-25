4b501fe161bb0858dc62baaae79efc2265073a6f
Ivaylо <ivaylo.a.karakashev@gmail.com>
2023-03-25 20:47:06 +0200

    Formatted 'Terms and Conditions' text properly

--- firstProject/web/views/generic.py
+++ firstProject/web/views/generic.py
@@ -3,12 +3,14 @@ from django.shortcuts import render
 
 from firstProject.accounts.forms import SubscribeToNewsletterForm
 from firstProject.accounts.models import Profile, Subscribers
+from firstProject.web.models import Category
 
 
 def home_view(request, profile=None):
     page_title = 'Apparel Shop'
     form = None
     user_is_subscribed = Subscribers.objects.filter(subscribed_user_id=request.user.id)
+    new_categories = Category.objects.order_by('-id')[:3]
 
     if request.user.is_authenticated and not request.user.is_superuser:
         profile = Profile.objects.get(user_id=request.user.id)
@@ -28,6 +30,7 @@ def home_view(request, profile=None):
         'pofile': profile,
         'page_title': page_title,
         'subscription_form': form,
-        'user_is_subscribed': user_is_subscribed
+        'user_is_subscribed': user_is_subscribed,
+        'new_categories': new_categories
     }
     return render(request, 'front-end/index.html', context)
